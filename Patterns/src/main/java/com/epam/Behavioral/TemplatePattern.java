1dd8d88777aa76b180dd184d4e4091142088d5fc
Shiva Kiran <rskreddy8019@gmail.com>
2020-03-10 23:09:19 +0530

    BehavioralPattern

---
+++ Patterns/src/main/java/com/epam/Behavioral/TemplatePattern.java
@@ -0,0 +1,33 @@
+package com.epam.Behavioral;
+
+abstract class Exam {  
+    abstract void start();  
+    abstract void end();  
+    abstract void result();
+    public final void process(){    
+       start();  
+       end();  
+       result();
+    }  
+}
+class MathExam extends Exam{
+	void start(){
+		System.out.println("Math Exam started");
+	}
+	void end() {
+		System.out.println("Math Exam ended");
+	}
+	void result() {
+		System.out.println("Math Exam results have been declared");
+	}
+}
+
+public class TemplatePattern {
+	public static void main(String[] args) {
+		MathExam m=new MathExam();
+		m.start();
+		m.end();
+		m.result();
+
+	}
+}
